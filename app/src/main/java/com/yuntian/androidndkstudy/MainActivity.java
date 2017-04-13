1575aaa61d34618cee0a2768c46f9c9d58227efa
guangleilei <944610721@qq.com>
2017-04-13 22:57:02 +0800

    修改打印日志

--- app/src/main/java/com/yuntian/androidndkstudy/MainActivity.java
+++ app/src/main/java/com/yuntian/androidndkstudy/MainActivity.java
@@ -3,7 +3,6 @@ package com.yuntian.androidndkstudy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
